a61776fa6ccedd9a10285ece66be49b678f646a8
TharunKumarReddy62 <246526024+TharunKumarReddy62@users.noreply.github.com>
2026-07-06 17:09:37 +0530

    Initial commit

---
+++ src/main/java/com/project/EcommerceHub/EcommerceHubApplication.java
@@ -0,0 +1,13 @@
+package com.project.EcommerceHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceHubApplication.class, args);
+	}
+
+}
