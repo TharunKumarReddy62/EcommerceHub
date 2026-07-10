6e8283252f142c809bc843804f6c3dd90c82321c
TharunKumarReddy62 <246526024+TharunKumarReddy62@users.noreply.github.com>
2026-07-10 21:41:36 +0530

    Completed product module  implementation

---
+++ src/main/java/com/project/EcommerceHub/repositories/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.project.EcommerceHub.repositories;
+
+
+import com.project.EcommerceHub.model.Category;
+import com.project.EcommerceHub.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryOrderByPriceAsc(Category category, Pageable pageDetails);
+
+    Page<Product> findByProductNameLikeIgnoreCase(String keyword, Pageable pageDetails);
+}
+
